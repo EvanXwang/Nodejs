69b619bdf47bc7515a40bf8ef7a9d67195d379a2
Evan Wang <wangxevan@gmail.com>
2021-04-18 15:18:43 +0800

    update

--- Training/hello_world.js
+++ Training/hello_world.js
@@ -1,11 +1,14 @@
+//透過http 模組啟動 web server 服務
 const http = require('http');
 
+//設定服務監聽localhost:3000 (127.0.0.1:3000/
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
+  //設定回應為text文件，並回應 Hello World
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
