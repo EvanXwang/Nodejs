28ba52091fc3b19ab242da67ee88aa84aecf217c
Evan Wang <wangxevan@gmail.com>
2021-04-26 15:05:59 +0800

    新增模組化開發

---
+++ Training/node_module_exports/a.js
@@ -0,0 +1,9 @@
+//使用require 將b.js 引入
+var b = require('./b')
+
+
+//多筆變數或函數
+console.log(b.myName1);
+console.log(b.myName2);
+console.log(b.sayHi());
+console.log(b.num(3));
---
+++ Training/node_module_exports/b.js
@@ -0,0 +1,26 @@
+var myName = 'Ray';
+
+//模組化 myName此變數
+module.exports = myName;
+
+
+
+//若有多個變數或函數想傳入可這樣寫：
+
+var myName1 = 'Evan';
+var myName2 = 'Jojo';
+
+function sayHi(){
+    return 'Hello'+myName;
+}
+
+function num(n){
+    return n * 2 ;
+}
+
+module.exports = {
+    myName1,
+    myName2,
+    sayHi,
+    num
+};
